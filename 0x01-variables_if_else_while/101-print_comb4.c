2917c364884a6d82349c4ac569a07ad194e7d254
Ameena Nelson <ameena12nelson@gmail.com>
2022-12-09 08:44:34 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,23 +7,23 @@
 
 int main(void)
 {
-	int first_digit = '0';
-	int second_digit = '0';
-	int third_digit = '0';
+	int ft = '0';
+	int sd = '0';
+	int td = '0';
 
-	for (third_digit = '0'; third_digit <= '9'; third_digit++)
+	for (td = '0'; td <= '9'; td++)
 	{
-		for (second_digit = '0'; second_digit <= '9'; second_digit++)
+		for (sd = '0'; sd <= '9'; sd++)
 		{
-			for (first_digit = '0'; first_digit <= '9'; first_digit++)
+			for (ft = '0'; ft <= '9'; ft++)
 			{
-				if (!((first_digit == second_digit) || (second_digit == third_digit) || (third_digit > second_digit)))
+				if (!((ft == sd) || (sd == td) || (sd > ft) || (td > sd)))
 				{
-					putchar(third_digit);
-					putchar(second_digit);
-					putchar(first_digit);
+					putchar(td);
+					putchar(sd);
+					putchar(ft);
 
-					if (!(first_digit == '9' && third_digit == '7' && second_digit == '8'))
+					if (!(ft == '9' && td == '7' && sd == '8'))
 					{
 						putchar(',');
 						putchar(' ');
