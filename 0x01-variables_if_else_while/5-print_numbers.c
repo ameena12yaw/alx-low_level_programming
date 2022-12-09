93cd573a54fa1a6447ea9f03cbc317bbd8dcc82f
Ameena Nelson <ameena12nelson@gmail.com>
2022-12-09 00:21:35 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from 0 - 9
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for number = 0; number < 10; number++)
+		printf("%d", number);
+
+	printf("\n");
+
+	return (0);
+}
