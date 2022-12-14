63f3c5140102cb85a1b3649a6ead627951576c07
Ameena Nelson <ameena12nelson@gmail.com>
2022-12-14 00:21:57 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - creates an finite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -15,7 +15,7 @@ i = 0;
 /*
  * while (i < 10)
  * {
- * 		putchar(i);
+ * putchar(i);
  * }
  */
 printf("Infinite loop avoided! \\o/\n");
