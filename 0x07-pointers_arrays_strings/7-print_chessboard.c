0a2d790389f9127fc9a330c07221673de3c156df
Ameena Nelson <ameena12nelson@gmail.com>
2023-01-03 06:01:50 -0800

    a function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_chessboard - prints the chessboard
+ * @a: input pointer
+ * Rrturn: ALways 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int c, b = 0;
+
+	for (c = 0; c < 64; c++)
+	{
+		if (c % 8 == 0 && c != 0)
+		{
+			b = c;
+			_putchar('\n');
+		}
+		_putchar(a[c / 8][c - m]);
+	}
+	_putchar('\n');
+}
