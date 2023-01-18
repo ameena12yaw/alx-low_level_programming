e01e58fde3443bcad1ec44df1bd902adf253cf5a
Ameena Nelson <ameena12nelson@gmail.com>
2023-01-18 04:34:34 -0800

    a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - prints name
+ * @name: pointer name
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name,  void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
