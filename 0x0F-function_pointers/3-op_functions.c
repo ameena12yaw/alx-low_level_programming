40ee714d7d763dd2ea7c1bdabd68a39eabb70f38
Ameena Nelson <ameena12nelson@gmail.com>
2023-01-18 07:57:36 -0800

    a program that performs simple operations.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,10 @@
-#include "3-calc.h"
+#include "3-cialc.h"
 
 /**
  * op_add - return the sum of 2 numbers
- * @a: integer
- * @b: integer
-  * Return: sun
+ * @a: first integer
+ * @b: second integer
+  * Return: sun of a and b
  */
 
 int op_add(int a, int b)
@@ -14,9 +14,9 @@ int op_add(int a, int b)
 
 /**
  * op_sub - returns difference of 2 numbers
- * @a: integer
- * @b: integer
- * Return: Difference
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
  */
 
 int op_sub(int a, int b)
@@ -26,9 +26,9 @@ int op_sub(int a, int b)
 
 /**
  * op_mul - returns product of 2 numbers
- * @a: interger
- * @b: integer
- * Return: product
+ * @a: first interger
+ * @b: second integer
+ * Return: product of a and b
  */
 
 int op_mul(int a, int b)
@@ -37,22 +37,28 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - returns the quotient of 2 numbers
- * @a: integer
- * @b: integer
- * Return: quotient
+ * op_div - returns the division of 2 numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: the division of a and b
  */
 
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
+/**
+ * op_mod -returns the remainder of the division of 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of a divided b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
 
 
 
