ff6492c5c8dd8140a51821ef661a512fc4bfea39
Ameena Nelson <ameena12nelson@gmail.com>
2023-01-18 05:50:46 -0800

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,85 @@
+#include "3-calc.h"
+
+/**
+ * op_add - return the sum of 2 numbers
+ * @a: integer
+ * @b: integer
+ * Return: sun
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference of 2 numbers
+ * @a: integer
+ * @b: integer
+ * Return: Difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product of 2 numbers
+ * @a: interger
+ * @b: integer
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the quotient of 2 numbers
+ * @a: integer
+ * @b: integer
+ * Return: quotient
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
