0e6f6c29925b23056b507fedc6f0900ae48837bf
Ameena Nelson <ameena12yaw@gmail.com>
2023-02-06 04:58:16 -0800

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int a, b, length = 0;
+	int led;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (length = 0; text_content[length];)
-			length++;
-	}
+	led = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	b = write(a, text_content, length);
-
-	if (a == -1 || b == -1)
+	if (led == -1)
 		return (-1);
 
-	close(a);
+	if (text_content)
+		write(led, text_content, _strlrn(text_content));
 
+
+	close(led);
 	return (1);
 }
 
